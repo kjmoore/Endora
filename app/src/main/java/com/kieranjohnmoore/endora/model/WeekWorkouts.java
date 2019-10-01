b428a634abbb6cc4a09e85fae3f1b4a1c6e02481
Kieran Moore <kieranjohnmoore@gmail.com>
2019-10-01 22:21:41 +0100

    Adding in the layout for the weeks workouts

---
+++ app/src/main/java/com/kieranjohnmoore/endora/model/WeekWorkouts.java
@@ -0,0 +1,28 @@
+package com.kieranjohnmoore.endora.model;
+
+public class WeekWorkouts {
+    public class Day {
+        public String name;
+        public boolean completed = false;
+
+        Day(String name, boolean completed) {
+            this.name = name;
+            this.completed = completed;
+        }
+    }
+    public Day[] days = new Day[7];
+
+    WeekWorkouts() {
+        for (int i = 0; i < days.length; i++) {
+            days[i] = new Day("", false);
+        }
+        days[0].name = "Mon"; days[0].completed = false;
+        days[1].name = "Tues"; days[1].completed = true;
+        days[2].name = "Wed"; days[2].completed = false;
+        days[3].name = "Thur"; days[3].completed = true;
+        days[4].name = "Fri"; days[4].completed = true;
+        days[5].name = "Sat"; days[5].completed = true;
+        days[6].name = "Sun"; days[7].completed = false;
+    }
+
+}
